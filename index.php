9e2819133a117b01b9400ea14241b2913e119db7
Kanako-Imai <s0502027@yahoo.co.jp>
2024-01-16 20:52:37 +0900

    index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 <ul>
 <li><a href="get.php">form(get)</a></li>
-<li><a href="post.php">form(post)</a></li>
+<li><a href="post01.php">form(post)</a></li>
 <li><a href="hensu.php">変数</a></li>
 <li><a href="hairetsu.php">配列</a></li>
 <li><a href="kansu.php">関数</a></li>
@@ -14,7 +14,7 @@
 </ul>
 <ul>
 <li><a href="write01.php">ファイル書き込み</a></li>
-<li><a href="read.php">ファイル読み込む</a></li>
+<li><a href="read01.php">ファイル読み込む</a></li>
 </ul>
 </body>
 </html>
